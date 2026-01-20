1e57fb540a8ae1ebbedbaf5a36b87399e4d6dd0a
Omkharade318 <kharadeoms@gmail.com>
2026-01-20 13:01:24 +0530

    Completed design of SignUp Screen

--- app/src/main/java/com/example/appetito/ui/Widgets.kt
+++ app/src/main/java/com/example/appetito/ui/Widgets.kt
@@ -1,6 +1,7 @@
 package com.example.appetito.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,19 +11,31 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.appetito.R
+import com.example.appetito.ui.theme.Orange
 
 
 @Composable
@@ -86,5 +99,72 @@ fun SocialButton(
 
             Text(text = stringResource(id = title), color = Color.Black)
         }
+    }
+}
+
+@Composable
+fun FoodHubTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = RoundedCornerShape(12.dp),
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors().copy(
+        focusedIndicatorColor = Orange,
+        unfocusedIndicatorColor = Color.LightGray.copy(alpha = 0.4f),
+    )
+){
+
+    Column(modifier = Modifier.padding(vertical = 8.dp)) {
+        label?.let {
+            Row {
+                Spacer(modifier = Modifier.size(4.dp))
+                it()
+            }
+        }
+
+        Spacer(modifier = Modifier.size(8.dp))
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = modifier.height(65.dp),
+            enabled = enabled,
+            readOnly = readOnly,
+            textStyle = textStyle.copy(fontWeight = FontWeight.SemiBold),
+            label = null,
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            prefix = prefix,
+            suffix = suffix,
+            supportingText = supportingText,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            minLines = minLines,
+            interactionSource = interactionSource,
+            shape = shape,
+            colors = colors,
+        )
     }
 }
--- app/src/main/java/com/example/appetito/ui/features/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/appetito/ui/features/auth/signup/SignUpScreen.kt
@@ -12,8 +12,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,16 +26,33 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.appetito.R
+import com.example.appetito.ui.FoodHubTextField
 import com.example.appetito.ui.GroupSocialButtons
 import com.example.appetito.ui.theme.Orange
 
 @Composable
 fun SignUpScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
+
+        var name by remember {
+            mutableStateOf("")
+        }
+
+        var email by remember {
+            mutableStateOf("")
+        }
+
+        var password by remember {
+            mutableStateOf("")
+        }
+
+
         Image(
             painter = painterResource(id = R.drawable.auth_bg),
             contentDescription = null,
@@ -47,16 +69,83 @@ fun SignUpScreen() {
 
             Box(modifier = Modifier.weight(1f))
 
+            Text(
+                text = stringResource(R.string.sign_up),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.size(20.dp))
+
+            FoodHubTextField(
+                value = name,
+                onValueChange = {name = it},
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.full_name),
+                        color = Color.Gray,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 16.sp
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            FoodHubTextField(
+                value = email,
+                onValueChange = {email = it},
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.email),
+                        color = Color.Gray,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 16.sp
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            FoodHubTextField(
+                value = password,
+                onValueChange = {password = it},
+                label = {
+                    Text(text = stringResource(
+                        id = R.string.password),
+                        color = Color.Gray,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 16.sp
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation(),
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_eye),
+                        contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                }
+            )
+
+            Spacer(modifier = Modifier.size(16.dp))
+
             Button(
                 onClick = {},
                 modifier = Modifier
-                    .height(48.dp),
+                    .height(60.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Orange)
             ) {
                 Text(
                     text = stringResource(id = R.string.sign_up),
-                    modifier = Modifier.padding(horizontal = 32.dp),
-                    fontWeight = FontWeight.SemiBold
+                    modifier = Modifier
+                        .padding(horizontal = 48.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp
                 )
 
             }
